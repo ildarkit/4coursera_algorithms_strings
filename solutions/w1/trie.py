caefdde4476898636f91a598c5f5a01d331905f9
ILdar Shamiev <shamief@yandex.ru>
2018-08-01 15:59:18 +0300

    w1 sol1 Construct a trie from a collection of patterns implemented, grader tests passed

--- solutions/w1/trie.py
+++ solutions/w1/trie.py
@@ -1,19 +1,34 @@
 # Uses python3
 import sys
 
-# Return the trie built from patterns
-# in the form of a dictionary of dictionaries,
-# e.g. {0:{'A':1,'T':2},1:{'C':3}}
-# where the key of the external dictionary is
-# the node ID (integer), and the internal dictionary
-# contains all the trie edges outgoing from the corresponding
-# node, and the keys are the letters on those edges, and the
-# values are the node IDs to which these edges lead.
-
 
 def build_trie(patterns):
+    """
+    Return the trie built from patterns
+    in the form of a dictionary of dictionaries,
+    e.g. {0:{'A':1,'T':2},1:{'C':3}}
+    where the key of the external dictionary is
+    the node ID (integer), and the internal dictionary
+    contains all the trie edges outgoing from the corresponding
+    node, and the keys are the letters on those edges, and the
+    values are the node IDs to which these edges lead.
+    """
     tree = dict()
     # write your code here
+    i = 0
+    for pattern in patterns:
+        current_node = 0
+        for c in pattern:
+            current_symbol = c
+            if current_symbol in tree.get(current_node, {}):
+                current_node = tree[current_node][current_symbol]
+            else:
+                i += 1
+                if current_node not in tree:
+                    tree[current_node] = {c: i}
+                else:
+                    tree[current_node].update({c: i})
+                current_node = i
     return tree
 
 
