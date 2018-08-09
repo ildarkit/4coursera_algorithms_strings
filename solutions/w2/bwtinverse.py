6f6d4042353bf317d48a40baea03b464444da3e1
ILdar Shamiev <shamief@yandex.ru>
2018-08-09 19:04:35 +0300

    w2 sol2 added starter file

---
+++ solutions/w2/bwtinverse.py
@@ -0,0 +1,12 @@
+# python3
+import sys
+
+
+def inverse_btw(bwt):
+    # write your code here
+    return ""
+
+
+if __name__ == '__main__':
+    bwt = sys.stdin.readline().strip()
+    print(inverse_btw(bwt))
