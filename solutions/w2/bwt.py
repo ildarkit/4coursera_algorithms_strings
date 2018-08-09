cf0fc2653005a85227c5bc716966f88b66d08863
ILdar Shamiev <shamief@yandex.ru>
2018-08-09 17:16:15 +0300

    w2 sol1 implemented Burrows–Wheeler transform, grader tests passed

--- solutions/w2/bwt.py
+++ solutions/w2/bwt.py
@@ -3,7 +3,33 @@ import sys
 
 
 def bwt(text):
-    return ""
+    """
+    Implementation of Burrows–Wheeler transform of text.
+    :param text: Text contains symbols A, C, G, T, $ only.
+    :return: Burrows–Wheeler transform
+    """
+    text = list(text)
+    matrix = rotation(text)
+    matrix = sorted(matrix, key=sorting)
+    # The last column is bwt
+    bwtransform = [row[-1] for row in matrix]
+    return "".join(bwtransform)
+
+
+def rotation(text):
+    matrix = []
+    for i in range(len(text)):
+        matrix.append(text)
+        new_text = text[:]
+        last = new_text.pop()
+        new_text.insert(0, last)
+        text = new_text
+    return matrix
+
+
+def sorting(text):
+    index = text.index('$')
+    return text[:index]
 
 
 if __name__ == '__main__':
