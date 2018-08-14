574b112d48442d7fdc94da05ae304f9889598632
ILdar Shamiev <shamief@yandex.ru>
2018-08-14 19:45:43 +0300

    w2 sol2 the first column is sorted by counting, grader tests not passed?

--- solutions/w2/bwtinverse.py
+++ solutions/w2/bwtinverse.py
@@ -1,53 +1,46 @@
 # python3
 import sys
+from collections import OrderedDict
 
 
-def inverse_btw(bwt):
+def inverse_bwt(bwt):
     """
     Reconstructing a string from its Burrows–Wheeler transform.
     :param bwt: Burrows–Wheeler transform
     :return: string
     """
-    last_column = [(bwt[i], i) for i in range(len(bwt))]
-    first_column = build_first_column(last_column)
+    count = {'$': 0, 'A': 0, 'C': 0, 'G': 0, 'T': 0}
+    count = OrderedDict(count)
+    num = {'$': 0, 'A': 1, 'C': 2, 'G': 3, 'T': 4}
+    first_column = count_sort(bwt, count, num)
     i = 0
     result = []
     while True:
-        item_in_last_column = last_column[i]
-        if item_in_last_column[0] != '$':
-            result.insert(0, item_in_last_column[0])
+        item_in_last_column = bwt[i]
+        if item_in_last_column != '$':
+            result.insert(0, item_in_last_column)
         else:
-            result.append(item_in_last_column[0])
+            result.append(item_in_last_column)
             break
-        i = first_column[item_in_last_column]
+        i = first_column[(item_in_last_column, i)]
 
     return "".join(result)
 
 
-def sorting(item):
-    """
-    Sorting by character.
-    :param item: tuple of character and index of list
-    :return: character
-    """
-    return item[0]
-
-
-def build_first_column(last_column):
-    """
-    The construction of the first column by sorting bwt.
-    :param last_column: bwt
-    :return: first column
-    """
-    first_column = {}
-    # index "i" corresponds to the index in the last column
-    i = 0
-    for item in sorted(last_column, key=sorting):
-        first_column[item] = i
-        i += 1
-    return first_column
+def count_sort(seq, count, num):
+    sorted_seq = dict()
+    for symbol in seq:
+        count[symbol] = count[symbol] + 1
+    pos = [0 for _ in range(len(count))]
+    for i, key in enumerate(count):
+        if i < len(count) - 1:
+            pos[i + 1] = pos[i] + count[key]
+    for i in range(len(seq)):
+        sorted_seq[(seq[i], i)] = pos[num[seq[i]]]
+        pos[num[seq[i]]] = pos[num[seq[i]]] + 1
+    return sorted_seq
 
 
 if __name__ == '__main__':
     bwt = sys.stdin.readline().strip()
-    print(inverse_btw(bwt))
+    print(inverse_bwt(bwt))
